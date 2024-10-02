f65dbe66b0ea760d48802b3af4d431d0c37b15d6
Yann-Thomas Le Moigne <yann.tho.le.moigne@gmail.com>
2024-10-02 12:19:26 +0200

    Correction TU

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -2,6 +2,8 @@ import { TestBed } from '@angular/core/testing';
 import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 import { Component } from '@angular/core';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { AppMaterialModule } from './app-material.module';
 
 @Component({
   selector: 'crm-login',
@@ -12,7 +14,11 @@ class LoginTestComponent {}
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [RouterModule.forRoot([])],
+      imports: [
+        RouterModule.forRoot([]),
+        NoopAnimationsModule,
+        AppMaterialModule,
+      ],
       declarations: [AppComponent, LoginTestComponent],
     }).compileComponents();
   });
--- src/app/login/login.component.spec.ts
+++ src/app/login/login.component.spec.ts
@@ -2,6 +2,8 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { LoginComponent } from './login.component';
 import { ReactiveFormsModule } from '@angular/forms';
+import { AppMaterialModule } from '../app-material.module';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 
 describe('LoginComponent', () => {
   let component: LoginComponent;
@@ -10,7 +12,7 @@ describe('LoginComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [LoginComponent],
-      imports: [ReactiveFormsModule],
+      imports: [ReactiveFormsModule, AppMaterialModule, NoopAnimationsModule],
     }).compileComponents();
 
     fixture = TestBed.createComponent(LoginComponent);
