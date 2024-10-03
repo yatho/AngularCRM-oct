42de15e71fd7314aa99f0ee2c482da33bfd6163e
Yann-Thomas Le Moigne <yann.tho.le.moigne@gmail.com>
2024-10-03 10:48:17 +0200

    Ajout du proxy conf

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { DemoObservableService } from '../common/demo-observable.service';
+import { map, Observable, take } from 'rxjs';
 
 @Component({
   selector: 'crm-home',
   templateUrl: './home.component.html',
-  styleUrl: './home.component.scss'
+  styleUrl: './home.component.scss',
 })
 export class HomeComponent {
+  demoObs = inject(DemoObservableService);
 
+  testObservable(): void {
+    const obs: Observable<number> = this.demoObs.getObservable();
+
+    const subscription = obs.subscribe((result: number) => {
+      console.log('result = ', result);
+    });
+
+    setTimeout(() => {
+      subscription.unsubscribe();
+    }, 1500);
+  }
 }
